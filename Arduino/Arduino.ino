ce8706d798acf8d1558d0460a8b52c6f3baead54
Chris Kingdon <cdkingdon@gmail.com>
2024-02-25 17:04:36 -0500

    adding arduino code

---
+++ Arduino/Arduino.ino
@@ -0,0 +1,61 @@
+/*
+
+*/
+
+#include "definitions.h"
+#include <IRremote.hpp>
+#include <serial-readline.h>
+
+typedef enum SerialCommand {
+  IR_POWER_OFF = 0,
+  IR_POWER_ON = 1,
+  IR_COLOR_RED = 2,
+  IR_COLOR_GREEN = 3,
+  IR_COLOR_PURPLE = 4
+};
+
+void received(char*);
+
+SerialLineReader reader(Serial, received);
+
+void setup() {
+  Serial.begin(115200);
+  
+  // Start the receiver and if not 3. parameter specified, take LED_BUILTIN pin from the internal boards definition as default feedback LED
+  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
+  Serial.println("Ready to receive IR signals at pin " + String(IR_RECEIVE_PIN));
+
+  IrSender.begin(ENABLE_LED_FEEDBACK);
+  Serial.println("Ready to send IR signals at pin " + String(IR_SEND_PIN));
+}
+
+void loop() {
+  // Handle serial input
+  reader.poll();
+
+  // Handle IR input
+  if (IrReceiver.decode()) {
+    Serial.println("IR command recieved, forwarding... ");
+    IrReceiver.printIRResultShort(&Serial);
+    Serial.println();
+
+    // Repeater
+    IrSender.sendNEC(IrReceiver.decodedIRData.address, IrReceiver.decodedIRData.command, 1);
+    IrReceiver.resume(); // Enable receiving of the next value
+  }
+}
+
+void received(char *line) {
+  String stringLine = String(line);
+  Serial.println("Serial command recieved: " + stringLine);
+
+  switch (stringLine.toInt()) {
+    case IR_POWER_OFF: IrSender.sendNEC(IR_DEFAULT_ADDRESS_HEX, IR_POWER_OFF_HEX, 1); break;
+    case IR_POWER_ON: IrSender.sendNEC(IR_DEFAULT_ADDRESS_HEX, IR_POWER_ON_HEX, 1); break;
+    case IR_COLOR_RED: IrSender.sendNEC(IR_DEFAULT_ADDRESS_HEX, IR_COLOR_RED_HEX, 1); break;
+    case IR_COLOR_GREEN: IrSender.sendNEC(IR_DEFAULT_ADDRESS_HEX, IR_COLOR_GREEN_HEX, 1); break;
+    case IR_COLOR_PURPLE: IrSender.sendNEC(IR_DEFAULT_ADDRESS_HEX, IR_COLOR_PURPLE_HEX, 1); break;
+    default: break;
+  }
+  
+}
---
+++ Arduino/definitions.h
@@ -0,0 +1,10 @@
+#define IR_RECEIVE_PIN      2
+#define IR_SEND_PIN         3
+#define ENABLE_LED_FEEDBACK 1
+
+#define IR_POWER_ON_HEX         0xD
+#define IR_POWER_OFF_HEX        0x1F
+#define IR_DEFAULT_ADDRESS_HEX  0x0
+#define IR_COLOR_RED_HEX        0x19
+#define IR_COLOR_GREEN_HEX      0x1B
+#define IR_COLOR_PURPLE_HEX     0x4
